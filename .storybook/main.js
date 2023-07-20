675ebdc1020c3964f414a453ecc990b5e5d05ab4
cmundados <lukas.mikovec@gmail.com>
2023-07-20 16:05:56 +0200

    automigration repair

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,21 +1,18 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
-  stories: [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
-  ],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
-    "@storybook/addon-interactions",
-    "@storybook/storybook-addon-designs"
-  ],
+  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-onboarding", "@storybook/addon-interactions", "@storybook/storybook-addon-designs", "@storybook/addon-mdx-gfm"],
   reactOptions: {
-    fastRefresh: true,
+    fastRefresh: true
   },
   docs: {
-    autodocs: "tag",
+    autodocs: "tag"
   },
+  framework: {
+    name: "@storybook/react-vite",
+    options: {
+      fastRefresh: true
+    }
+  }
 };
 export default config;
